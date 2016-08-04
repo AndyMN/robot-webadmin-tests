509e939dd35ce4ee4033b4d1070d7c948a390f05
root <root@vm-dcache-wn-sl7.desy.de>
2016-08-04 18:06:20 +0200

    Added a PageName:ElementID dict

--- Variables.py
+++ Variables.py
@@ -1,8 +1,11 @@
 
-
+# Title is pagetitle whenever we click on given element
 ElemID_Title_nologin = {"nav.home":"dCache Overview", "nav.cells":"Services", "nav.pools":"Pool Usage", "nav.poolqueues":"Pool Request Queues", "nav.poolqueueplots":"Pool Queue Activity Plots", "nav.poolgroup":"Pool Property Tables", "nav.tapetransfers":"dCache Dataset Restore Monitor", "nav.activetransfers":"Active Transfers", "nav.billing":"Billing History Plots", "nav.poolselection":"PoolManager (Pool SelectionUnit) Configuration", "nav.space":"Space Tokens", "nav.pooladmin":"Login", "nav.celladmin":"Login", "nav.alarms":"Login"}
 
 ElemID_Title_admin = dict(ElemID_Title_nologin)
 ElemID_Title_admin["nav.pooladmin"] = "Pool Admin" 
 ElemID_Title_admin["nav.celladmin"] = "Cell Admin"
 ElemID_Title_admin["nav.alarms"] = "DCache Alarm Page"
+
+# PageName is name that is written on the buttons to go to each page
+PageName_ElemID_dict = {"Home":"nav.home", "Cell Services":"nav.cells", "Pool Usage":"nav.pools", "Pool Queues":"nav.poolqueues", "Pool Queue Plots":"nav.poolqueueplots", "Poolgroups":"nav.poolgroup", "Tape Transfer Queue":"nav.tapetransfers", "Active Transfers":"nav.activetransfers", "Billing Plots":"nav.billing", "Pool Selection Setup":"nav.poolselection", "Pool Admin":"nav.pooladmin", "Cell Admin":"nav.celladmin", "Space Tokens":"nav.space", "Alarms":"nav.alarms"}
